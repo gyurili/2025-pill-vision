e91dc2f85d23a7e06e39ee5546c9bb691926af88
kyakyak <udosjdjdjdj@gmail.com>
2025-03-18 03:12:45 +0900

    컨벤션에 맞게 코드 정리

--- src/visualization.py
+++ src/visualization.py
@@ -2,31 +2,39 @@ import matplotlib.pyplot as plt
 import cv2
 from dataset import convert_bbox_format
 
+
 def visualize_sample(image, image_vis, target, class_id=False, bbox_convert=True):
     """
     바운딩 박스를 시각화하는 함수.
+
     Args:
-        image (torch.Tensor): 모델 입력용 텐서
-        image_vis (numpy.ndarray): 정규화 해제된 NumPy 이미지
-        target (dict): 바운딩 박스 정보
-        class_id (list, optional): 클래스 ID를 출력 하는 것에 대한 불 값
+        image (torch.Tensor): 모델 입력용 텐서.
+        image_vis (numpy.ndarray): 정규화 해제된 NumPy 이미지.
+        target (dict): 바운딩 박스 정보 (COCO 또는 Pascal VOC 형식).
+        class_id (bool, optional): 클래스 ID를 출력할지 여부. Defaults to False.
+        bbox_convert (bool, optional): COCO 형식을 Pascal VOC로 변환할지 여부. Defaults to True.
+    
+    Returns:
+        None
     """
+    # 바운딩 박스 및 라벨 변환
     boxes = target["boxes"].cpu().numpy().astype(int)
     labels = target["labels"].cpu().numpy()
-    
-    if bbox_convert:
-        boxes = convert_bbox_format(boxes)
 
-    # 바운딩 박스 그리기
-    for i, (box, label) in enumerate(zip(boxes, labels)):
+    if bbox_convert:
+        boxes = convert_bbox_format(boxes, to_format="pascal")
+
+    # 바운딩 박스 시각화
+    for box, label in zip(boxes, labels):
         x_min, y_min, x_max, y_max = box
         cv2.rectangle(image_vis, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2)
 
         if class_id:
-            id = f"ID {label}"
-            cv2.putText(image_vis, id, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 
-                        0.5, (255, 0, 0), 2, cv2.LINE_AA)
+            class_text = f"ID {label}"
+            cv2.putText(image_vis, class_text, (x_min, y_min - 10), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
 
+    # 이미지 출력
     plt.figure(figsize=(8, 8))
     plt.imshow(image_vis)
     plt.axis("off")
