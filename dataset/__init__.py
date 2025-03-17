ec62cf2d9a37e2f445519406b05754ca47b30167
kyakyak <udosjdjdjdj@gmail.com>
2025-03-18 02:56:19 +0900

    bbox 형식 자유 변환 추가 / 기본 값은 coco형식으로 반환

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,2 +1,2 @@
 from .data_loader import get_dataloaders
-from .pill_dataset import PillDetectionDataset, TestDataset
+from .pill_dataset import PillDetectionDataset, TestDataset, convert_bbox_format
