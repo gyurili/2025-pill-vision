996308450cb86cb88d57f661ce8325c74f1251e1
gyurili <inglifestora@naver.com>
2025-03-18 07:26:37 +0000

    오류 수정

--- test2.py
+++ test2.py
@@ -6,16 +6,14 @@ from dataset import TestDataset
 from src import visualize_sample
 
 if __name__ == "__main__":
-    """테스트 스크립트: 모델 로드 및 예측"""
 
-    # 현재 파일이 위치한 디렉토리를 기준으로 경로 설정
     BASE_DIR = Path(__file__).resolve().parent
     TEST_DIR = "/content/drive/MyDrive/코드잇 초급 프로젝트/정리된 데이터셋/test_images"
     MODEL_PATH = os.path.join(BASE_DIR, "models/faster_rcnn_epoch5.pth")
 
     # 모델 불러오기
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    num_classes = 10  # 학습할 때 사용한 클래스 개수와 맞춰야 함!
+    num_classes = 82
     model = get_faster_rcnn_model(num_classes)
     model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
     model.to(device)
