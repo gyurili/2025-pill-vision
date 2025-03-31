9c382e741fbefdde84936272fd55f868c1eee7ef
박상범 <beom@bagsangbeom-ui-MacBookPro.local>
2025-03-31 10:49:37 +0900

    main.py updates

--- models/model_main.py
+++ models/model_main.py
@@ -3,24 +3,16 @@ import optuna
 import torch
 import os
 import sys
-
-# 프로젝트 루트 디렉터리 설정 (main 폴더 기준)
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.append(BASE_DIR)  # main 폴더를 sys.path에 추가
-
-# src에서 set_main_dir() 가져오기
-from src import set_main_dir  
-set_main_dir()  # main 폴더를 작업 디렉토리로 설정
-
 # 필요한 모듈 import
 from tuning import objective
 from train import train
 from evaluate import evaluate
 from src.config import TEST_IMAGE_PATH
 
-test_image_path = TEST_IMAGE_PATH
 
-def main():
+
+def model_main():
+    test_image_path = TEST_IMAGE_PATH
     parser = argparse.ArgumentParser(description="YOLO Hyperparameter Optimization")
     parser.add_argument("--epochs", type=int, default=20, help="Number of training epochs")  # 기본값 20
     args = parser.parse_args()
@@ -36,6 +28,3 @@ def main():
 
     model = train(study.best_params, args.epochs, device)  # argparse에서 받은 epochs 사용
     evaluate(model, test_image_path)
-
-if __name__ == "__main__":
-    main()
